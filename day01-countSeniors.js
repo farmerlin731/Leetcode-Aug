34719f1e03d6f60f06fcf937aa01cacd4e5f277a
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2024-08-01 15:25:40 +0800

    day01: 2678.Number of Senior Citizens_ver2 (8/01)

--- day01-countSeniors.js
+++ day01-countSeniors.js
@@ -5,4 +5,22 @@
  * @param {string[]} details
  * @return {number}
  */
-var countSeniors = function (details) {};
+var countSeniorsRegex = function (details) {
+  let count = 0;
+  const regex = /(\d{10})([MF])(\d{2})(\d{2})/;
+
+  details.forEach((detail) => {
+    let match = detail.match(regex);
+    if (match && match[3] >= 60) count++;
+  });
+
+  return count;
+};
+
+var countSeniors = function (details) {
+  return details.filter((el) => el.slice(-4, -2) > 60).length;
+};
+
+const details = ["7868190130M7522", "5303914400F9211", "9273338290F4010"];
+
+console.log(countSeniors(details));
