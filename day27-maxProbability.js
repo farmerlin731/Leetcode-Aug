8fc7a8aab7d36e5d581210c23716305a90486938
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2024-08-27 19:32:14 +0800

    day27: 1514.Path with Maximum Probability (8/27)

---
+++ day27-maxProbability.js
@@ -0,0 +1,36 @@
+//First Idea:
+//Dijkstra's algorithm?
+//https://leetcode.com/problems/path-with-maximum-probability/solutions/742828/very-efficient-and-easy-to-undersand-bfs
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} succProb
+ * @param {number} start_node
+ * @param {number} end_node
+ * @return {number}
+ */
+
+//TBD
+var maxProbability = function (n, edges, succProb, start, end) {
+  const p = Array(n).fill(0);
+  const graph = p.reduce((m, _, i) => m.set(i, []), new Map());
+  edges.forEach(([u, v], i) => {
+    graph.get(u).push([v, succProb[i]]);
+    graph.get(v).push([u, succProb[i]]);
+  });
+
+  const queue = [[start, 1]];
+  p[start] = 1;
+
+  for (let [node, currP] of queue) {
+    for (let [adj, nextP] of graph.get(node)) {
+      if (currP * nextP > p[adj]) {
+        p[adj] = currP * nextP;
+        queue.push([adj, p[adj]]);
+      }
+    }
+  }
+
+  return p[end];
+};
